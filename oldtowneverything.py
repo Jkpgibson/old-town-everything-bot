12e1829e70ae17b75292affa05d42b7e8373daaf
Joseph Gibson <jkpgibson@gmail.com>
2019-07-31 14:44:42 -0500

    wrong auth stuff

--- oldtowneverything.py
+++ oldtowneverything.py
@@ -2,10 +2,10 @@ import tweepy
 import urllib.request
 import json
 
-CONSUMER_KEY = "WDvB4FT1c67N2GBgijgC9a7tq"
-CONSUMER_SECRET = "2bKGOMkGkFzJCVW25BIiRIzS6yurUwY0hiKVfYzp4i7uVRasMr"
-ACCESS_TOKEN = "4560347689-eR1Jbr8XtKxrvS1ZzcqpBhfyV4OGHyxEj2jt171"
-ACCESS_TOKEN_SECRET = "7IbI6zPdIZ74g334GcY4y9aZR4UhfhU6zfMtffhTleehW"
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -16,3 +16,5 @@ api = tweepy.API(auth)
 req = urllib.request.urlopen("https://api.noopschallenge.com/wordbot?set=nouns").read()
 word = req.decode("utf-8")
 print(word[11:len(word)-3])
+
+api.update_status("I got the ", word, "in the back")
