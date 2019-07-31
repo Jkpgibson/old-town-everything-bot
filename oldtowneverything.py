7fb6e2d222587ed25b8463f23bed4a4f704d112c
Joseph Gibson <jkpgibson@gmail.com>
2019-07-31 14:57:38 -0500

    Complete Functionality

--- oldtowneverything.py
+++ oldtowneverything.py
@@ -2,10 +2,10 @@ import tweepy
 import urllib.request
 import json
 
-CONSUMER_KEY = ""
-CONSUMER_SECRET = ""
-ACCESS_TOKEN = ""
-ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = "dZVaXOw7japUrPNlfce4TApWI"
+CONSUMER_SECRET = "OHZrtcT1xgSJIokPL1QegpAFWbx7hFVJL9HB6vvg6sy7hEZWrR"
+ACCESS_TOKEN = "1156645481193377792-La6scf0bT1izzMkR7ez7BtTpaBEIdp"
+ACCESS_TOKEN_SECRET = "SDGmaw3oIwoDatYCEyeomTB7yAdnHiRnIAhbyNhGuf7dZ "
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -14,7 +14,6 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 req = urllib.request.urlopen("https://api.noopschallenge.com/wordbot?set=nouns").read()
-word = req.decode("utf-8")
-print(word[11:len(word)-3])
+word = req.decode("utf-8")[11:len(word)-3]
 
-api.update_status("I got the ", word, "in the back")
+api.update_status("I got the ", word[11:len(word)-3], "in the back")
